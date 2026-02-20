b3fab04df5a81056e2a3397c3a80ae731f5a6630
KrishIngle <krishingle07@gmail.com>
2026-02-20 05:53:27 +0000

    Add Assignment_13 to implement integer reversal functionality

---
+++ Assignments/Assignment_13.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int num, reversed = 0, remainder;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    while(num != 0){
+        remainder = num%10;
+        reversed = reversed*10 + remainder;
+        num /= 10;
+    }
+    printf("Reversed number is: %d", reversed);
+
+    return 0;
+}
