2caf85d6b6e7d711541159b85e3299418cadc552
Krishna Ingle <krishingle07@gmail.com>
2026-04-05 11:05:31 +0000

    ADD Program 1 to 4 in Week 8

---
+++ NPTEL ASSIGNMENTS/WEEK 8/Program 1.c
@@ -0,0 +1,23 @@
+// Write a C Program to find HCF of 4 given numbers using recursive functionWrite a C Program to find HCF of 4 given numbers using recursive function
+
+#include <stdio.h>
+int HCF(int, int);
+int main() {
+    int a, b, c, d, result;
+    printf("Enter 4 numbers: ");
+   scanf("%d %d %d %d", &a, &b, &c, &d);
+   result = HCF(HCF(a, b), HCF(c,d));
+   printf("The HCF is %d", result);
+}
+
+int HCF(int x, int y){
+   while (x != y){
+      if (x > y){
+           return HCF(x - y, y);
+       }
+       else{
+       return HCF(x, y - x);
+       }
+    }
+    return x;
+}
